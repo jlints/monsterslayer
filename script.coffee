af4e9531c1690aab190525c94c0bb3e8049c1a1e
nicohman <nico.hickman@gmail.com>
2013-05-14 20:07:20 -0700

    Added blood cloud, dead illustatiion, and more

--- script.coffee
+++ script.coffee
@@ -9,19 +9,20 @@ $(document).ready ->
 		$("#2").fadeIn('fast')
 		paper = Raphael(document.getElementById("2"), 600, 600)
 		person = paper.image("person.png", 150, 300, 400, 300)
-		goblin = paper.image("goblin.png", 250, 180, 300, 300)
-		word = paper.text( 350 , 220,possibleWords[usedUp])
-		word.attr("font-size", "30pt")
-		word.attr("font-family", "Acknowledgement")
-		$(document).keypress (e)->
-					key = String.fromCharCode(e.which)
-					donesoFar += key
-					alert donesoFar
-					alert possibleWords[usedUp]
-					if (donesoFar is possibleWords[usedUp])
-						++usedUp
+		goblin = paper.image("goblin/goblin.png", 230, 180, 300, 300)
+		goblin.health = 3
+		#word = paper.text( 350 , 220,possibleWords[usedUp])
+		#word.attr("font-size", "30pt")
+		#word.attr("font-family", "Acknowledgement")
+		goblin.click (e)->
+					if (goblin.health <= 0)
 						goblinDie = Raphael.animation({height:0, width:0})
 						goblin.animate(goblinDie)
+						deadGoblin = paper.image("goblin/goblindead1.jpg", 250, 180, 500, 300)
+						setTimeout ->
+							goblinDie = Raphael.animation({height:0, width:0}, 2000)
+							deadGoblin.animate(goblinDie)
+						, 1000
 						t = paper.text(300, 300, "On to the next Level!")
 						t.attr("font-family", "LeagueGothic")
 						t.attr("font-size", "50pt")
@@ -31,10 +32,10 @@ $(document).ready ->
 							$("#3").fadeIn('fast')
 							paper = Raphael(document.getElementById("3"), 600, 600)
 							person = paper.image("person.png", 150, 300, 400, 300)
-							goblin = paper.image("goblin2.png", 250, 180, 300, 300)
-							goblin2 = paper.image("goblin.png", 300, 180, 300, 300)
-							word = paper.text(possibleWords[usedUp],250, 170)
-							++usedUp						
+							goblin = paper.image("goblin/goblin2.png", 250, 180, 300, 300)
+							goblin2 = paper.image("goblin/goblin.png", 300, 180, 300, 300)
+							#word = paper.text(possibleWords[usedUp],250, 170)
+							#++usedUp						
 							goblin2.click ->
 								goblinDie = Raphael.animation({height:0, width:0})
 								goblin2.animate(goblinDie)
@@ -50,7 +51,15 @@ $(document).ready ->
 									$("#4").fadeIn('fast')
 									paper = Raphael(document.getElementById("4"), 600, 600)
 									person = paper.image("person.png", 150, 300, 400, 300)
-									goblin = paper.image("goblin2.png", 250, 180, 300, 300)
-									goblin2 = paper.image("goblin.png", 300, 180, 300, 300)
+									goblin = paper.image("goblin/goblin2.png", 250, 180, 300, 300)
+									goblin2 = paper.image("goblin/goblin.png", 300, 180, 300, 300)
 								,5000
 						, 5000
+					else
+						setTimeout ->
+							goblin.attr("src", "goblin/goblinhurt.png")
+						,100
+						setTimeout ->
+							goblin.attr("src", "goblin/goblin.png")
+						, 300
+						--goblin.health
--- script.js
+++ script.js
@@ -7,31 +7,32 @@
     usedUp = 0;
     donesoFar = "";
     return $(document).keydown(function(e) {
-      var goblin, paper, person, word;
+      var goblin, paper, person;
 
       $(document).unbind();
       $(".1").remove();
       $("#2").fadeIn('fast');
       paper = Raphael(document.getElementById("2"), 600, 600);
       person = paper.image("person.png", 150, 300, 400, 300);
-      goblin = paper.image("goblin.png", 250, 180, 300, 300);
-      word = paper.text(350, 220, possibleWords[usedUp]);
-      word.attr("font-size", "30pt");
-      word.attr("font-family", "Acknowledgement");
-      return $(document).keypress(function(e) {
-        var goblinDie, key, t;
+      goblin = paper.image("goblin/goblin.png", 230, 180, 300, 300);
+      goblin.health = 3;
+      return goblin.click(function(e) {
+        var deadGoblin, goblinDie, t;
 
-        key = String.fromCharCode(e.which);
-        donesoFar += key;
-        alert(donesoFar);
-        alert(possibleWords[usedUp]);
-        if (donesoFar === possibleWords[usedUp]) {
-          ++usedUp;
+        if (goblin.health <= 0) {
           goblinDie = Raphael.animation({
             height: 0,
             width: 0
           });
           goblin.animate(goblinDie);
+          deadGoblin = paper.image("goblin/goblindead1.jpg", 250, 180, 500, 300);
+          setTimeout(function() {
+            goblinDie = Raphael.animation({
+              height: 0,
+              width: 0
+            }, 2000);
+            return deadGoblin.animate(goblinDie);
+          }, 1000);
           t = paper.text(300, 300, "On to the next Level!");
           t.attr("font-family", "LeagueGothic");
           t.attr("font-size", "50pt");
@@ -43,10 +44,8 @@
             $("#3").fadeIn('fast');
             paper = Raphael(document.getElementById("3"), 600, 600);
             person = paper.image("person.png", 150, 300, 400, 300);
-            goblin = paper.image("goblin2.png", 250, 180, 300, 300);
-            goblin2 = paper.image("goblin.png", 300, 180, 300, 300);
-            word = paper.text(possibleWords[usedUp], 250, 170);
-            ++usedUp;
+            goblin = paper.image("goblin/goblin2.png", 250, 180, 300, 300);
+            goblin2 = paper.image("goblin/goblin.png", 300, 180, 300, 300);
             goblin2.click(function() {
               goblinDie = Raphael.animation({
                 height: 0,
@@ -69,11 +68,19 @@
                 $("#4").fadeIn('fast');
                 paper = Raphael(document.getElementById("4"), 600, 600);
                 person = paper.image("person.png", 150, 300, 400, 300);
-                goblin = paper.image("goblin2.png", 250, 180, 300, 300);
-                return goblin2 = paper.image("goblin.png", 300, 180, 300, 300);
+                goblin = paper.image("goblin/goblin2.png", 250, 180, 300, 300);
+                return goblin2 = paper.image("goblin/goblin.png", 300, 180, 300, 300);
               }, 5000);
             });
           }, 5000);
+        } else {
+          setTimeout(function() {
+            return goblin.attr("src", "goblin/goblinhurt.png");
+          }, 100);
+          setTimeout(function() {
+            return goblin.attr("src", "goblin/goblin.png");
+          }, 300);
+          return --goblin.health;
         }
       });
     });
